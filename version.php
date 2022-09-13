3fad54eee84a8adc4e7addd4606165171cfb45e5
katjahedemann <61024304+katjahedemann@users.noreply.github.com>
2022-09-13 13:43:36 +0200

    Update version.php

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_moodleoverflow';
-$plugin->version = 2022080909;
+$plugin->version = 2022091301;
 $plugin->release = 'v4.0-r1';
 $plugin->requires = 2020061500; // Requires Moodle 3.9+.
 $plugin->maturity = MATURITY_STABLE;
